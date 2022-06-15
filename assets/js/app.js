e3ab16c5ee21d1702181a0123cd65d3c1e9b23b3
jv-miranda <joao.vitor.miranda.siqueira@gmail.com>
2022-06-15 16:03:43 -0300

    Project Finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,40 @@
+const result01 = document.querySelector(`#result01`)
+const result02 = document.querySelector(`#result02`)
+
 function calculate() {
+  reset.classList.add(`active`)
+  const mainNumberInput01 = document.querySelector(`#main_number_input_01`)
+  const mainNumberInput02 = document.querySelector(`#main_number_input_02`)
+
   const bill = Number(document.querySelector(`#bill`).value)
   const tipPercentage = getTipPercentage()
   const numberOfPeople = Number(document.querySelector(`#numberOfPeople`).value)
   const tipAmountPerPerson = (bill * tipPercentage) / 100 / numberOfPeople
   const totalPerPerson = bill / numberOfPeople
-  const result01 = document.querySelector(`#result01`)
-  const result02 = document.querySelector(`#result02`)
-  
-  result01.innerHTML = tipAmountPerPerson.toFixed(2)
-  result02.innerHTML = totalPerPerson.toFixed(2)
+
+  if (bill <= 0) {
+    mainNumberInput01.classList.add(`error`)
+    mainNumberInput02.classList.remove(`error`)
+    if (numberOfPeople <= 0) {
+      mainNumberInput02.classList.add(`error`)
+    }
+  } else if (numberOfPeople <= 0) {
+    mainNumberInput02.classList.add(`error`)
+    mainNumberInput01.classList.remove(`error`)
+    if (bill <= 0) {
+      mainNumberInput01.classList.add(`error`)
+    }
+  } else if (
+    tipAmountPerPerson !== null &&
+    tipAmountPerPerson !== Infinity &&
+    totalPerPerson !== null &&
+    totalPerPerson !== Infinity
+  ) {
+    mainNumberInput01.classList.remove(`error`)
+    mainNumberInput02.classList.remove(`error`)
+    result01.innerHTML = tipAmountPerPerson.toFixed(2)
+    result02.innerHTML = totalPerPerson.toFixed(2)
+  }
 }
 
 function getTipPercentage() {
@@ -35,9 +61,25 @@ function uncheckRadioButtons() {
   }
 }
 
-function removeInputNumberValue() {
-  const percentageNumberInput = document.querySelector(
-    `#percentage_number_input`
-  )
+function removeInputNumberValue(inputSelector) {
+  const percentageNumberInput = document.querySelector(`${inputSelector}`)
   percentageNumberInput.value = ``
+}
+
+function resetApp() {
+  if ((reset.classList.contains(`active`))) {
+    const mainNumberInput01 = document.querySelector(`#main_number_input_01`)
+    const mainNumberInput02 = document.querySelector(`#main_number_input_02`)
+
+    uncheckRadioButtons()
+    removeInputNumberValue(`#percentage_number_input`)
+    removeInputNumberValue(`#bill`)
+    removeInputNumberValue(`#numberOfPeople`)
+
+    mainNumberInput01.classList.remove(`error`)
+    mainNumberInput02.classList.remove(`error`)
+    result01.innerHTML = `0.00`
+    result02.innerHTML = `0.00`
+    reset.classList.remove(`active`)
+  }
 }
